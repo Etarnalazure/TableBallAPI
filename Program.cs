b318c6972ea638a35593503cb33d62a9f6836e4e
Rasmus Jensen <RASMUSAZU@hotmail.com>
2023-09-24 13:47:19 +0200

    Changed Program to allow for API Calls

--- Program.cs
+++ Program.cs
@@ -21,15 +21,26 @@ builder.Services.AddScoped<IRepository<TeamBaseModel>, Repository<TeamBaseModel>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("AllowSpecificOrigin");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
